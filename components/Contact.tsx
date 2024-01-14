b76eb23560d9e5d1b82b06bfb18ba60e8a1add1d
Himanshu Mukhedkar <himanshumukhedkar@gmail.com>
2024-01-14 13:57:08 +0530

    Updated code

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
         </h1>
 
-        <h4>If you want us to work together, have any questions, my inbox is always open. Whether I just want to say hi, I'll try my best to get back to you! Cheers!</h4>
+        <h4>If you want us to work together, have any questions, my inbox is always open. Whether I just want to say hi, I&apos;ll try my best to get back to you! Cheers!</h4>
         <div className="flex justify-center items-center my-4">
         <a href="mailto:himanshumukhedkar@gmail.com" 
           className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
